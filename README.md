2e50d57b0526549d16428dd29be3ed7ec14dc8a7
Pradeep Kumar <pnd.rns1@gmail.com>
2022-12-26 20:16:10 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# pkdev-terraform-aws
+1. Go Inside the pkdev-infra directory
+2. Execute "terraform init"
+3. Execute terraform plan
+4. Execute terraform apply
+
+Before executing above terraform commands, please make sure you have terraform and aws cli has been installed on your system.
+Once Aws cli has been installed, make sure inside your home directory there is a folder called .aws. inside that directory you configure your aws credntials with profile. Once that is configured use that profile inside provisioner module of terraform.
---
+++ pkdev-infra/pkdev-infra.tf
@@ -0,0 +1,66 @@
+module "myprovider"  {
+   source = "../modules/aws-provider"
+}
+
+module "vpc-module" {
+  source = "../modules/vpc"
+  subnet_id = module.my-subnet.public-subnet
+}
+
+module "my-subnet" {
+  source = "../modules/subnet"
+  vpc_id = module.vpc-module.vpc_id // comes from vpc output.tf file and passing it to my-subnet module.
+}
+module "security-group" {
+   source = "../modules/security-group"
+   vpc_id = module.vpc-module.vpc_id 
+}
+module "ec2module" {
+  source = "../modules/ec2"
+  vpc_security_group_ids = ["${module.security-group.security_group_public}"]
+  subnet_id = module.my-subnet.public-subnet
+}
+
+
+output "jenkins_master_public_ip" {
+   description = "The Public IP assigned to the Jenkins master instance"
+   value = "${module.ec2module.master_public_ip}"
+}
+
+output "jenkins_slave_public_ip" {
+   description = "The Public IP assigned to the Jenkins slave instance"
+   value = "${module.ec2module.slave_public_ip}"
+}
+
+module "k8scluster" {
+  source = "../modules/ec2"
+  vpc_security_group_ids = ["${module.security-group.security_group_public}"]
+  subnet_id = module.my-subnet.public-subnet
+  instance_type = "t2.medium"
+  ami = "ami-0149b2da6ceec4bb0"
+}
+
+output "k8s_master_public_ip" {
+   description = "The Public IP assigned to the K8s master instance"
+   value = "${module.k8scluster.master_public_ip}"
+}
+
+output "k8s_slave_public_ip" {
+   description = "The Public IP assigned to the k8s slave instance"
+   value = "${module.k8scluster.slave_public_ip}"
+}
+
+output "security_group_id" {
+   value = "${module.security-group.security_group_public}"
+}
+
+output "vpc_id" {
+  value = "${module.vpc-module.vpc_id}"
+}
+
+output "subnet_id" {
+  value = "${module.my-subnet.public-subnet}"
+}
+
+
+
