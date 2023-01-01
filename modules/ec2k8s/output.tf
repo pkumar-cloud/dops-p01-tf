253150e2143ba77a360f161a11d53db984df7a28
Pradeep Kumar <pnd.rns1@gmail.com>
2022-12-31 19:36:16 -0800

    Updated

--- modules/ec2k8s/output.tf
+++ modules/ec2k8s/output.tf
@@ -1,9 +1,9 @@
 output "master_public_ip" {
    description = "The Public IP assigned to the instance"
-   value = try(aws_instance.k8sec2["k8smaster"].public_ip,"")
+   value = try(aws_instance.ec2k8s["k8smaster"].public_ip,"")
   }
 
 output "slave_public_ip" {
    description = "The Public IP assigned to the instance"
-   value = try(aws_instance.k8sec2["k8sslave"].public_ip,"")
+   value = try(aws_instance.ec2k8s["k8sslave"].public_ip,"")
   }
